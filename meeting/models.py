91ee413a24a832c620015b6363dde057b7dd06d0
JaewonLee11 <ddugi1102@gmail.com>
2024-10-15 16:30:05 +0900

    recording 페이지에서 자동 녹음 기능 녹음 종료 기능 삽입 및 recording2 페이지 연결

--- meeting/models.py
+++ meeting/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 import os
 
+class Recording(models.Model):
+    file_path = models.FileField(upload_to='recordings/')
+    created_at = models.DateTimeField(auto_now_add=True)
 
-class Post(models.Model):
-    title = models.CharField(max_length=30)
-    hook_text = models.TextField(max_length=100, blank=True)
-    content = models.TextField()
+    def __str__(self):
+        return f'Recording at {self.created_at}'
