23b5b3fdda23d1dcfb757b24ef04fc621f2c6bec
Sesame-Oil <777daepark@gmail.com>
2024-11-04 11:48:17 +0900

    model 서버 통신 구현

--- meeting/views.py
+++ meeting/views.py
@@ -10,6 +10,13 @@ from django.http import JsonResponse
 import os
 import json
 import markdown
+from dotenv import load_dotenv
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 
 
 def login_view(request):
@@ -146,6 +153,46 @@ def save_audio(request):
             return JsonResponse({'message': 'File uploaded successfully'}, status=200)
 
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+
+def verify_api_key(request):
+    api_key = request.data['api_key']
+    return api_key == API_KEY
+
+
+@api_view(['PATCH'])
+@permission_classes([AllowAny])
+@csrf_exempt
+def store_meeting_detail(request):
+    if not verify_api_key(request):
+        return Response({'error': 'Forbidden: Invalid API Key'}, status=403)
+
+    meeting_id = request.data['meeting_id']
+    meeting = Meeting.objects.get(id=meeting_id)
+    meeting.content = request.data['content']
+    meeting.save()
+
+    return Response({'message': 'Meeting detail updated successfully'}, status=200)
+
+
+@api_view(['PATCH'])
+@permission_classes([AllowAny])
+@csrf_exempt
+def store_meeting_summary(request):
+    if not verify_api_key(request):
+        return Response({'error': 'Forbidden: Invalid API Key'}, status=403)
+
+    meeting_id = request.data['meeting_id']
+    meeting = Meeting.objects.get(id=meeting_id)
+    meeting.summary = request.data['summary']
+    meeting.save()
+
+    return Response({'message': 'Meeting summary updated successfully'}, status=200)
 
 
 @csrf_exempt  # CSRF 검증을 우회합니다. 보안상 주의 필요
